7c6b2e57f7534f6b1d37369a8bd1ca309d2c2281
TSITCL\s152100 <sasamoto2016@gmail.com>
2017-11-10 13:42:19 +0900

    アニメーション付けたけどバグがある

--- MagicMastry/Assets/Scripts/TestMagus.cs
+++ MagicMastry/Assets/Scripts/TestMagus.cs
@@ -14,6 +14,9 @@ public class TestMagus : MonoBehaviour
     public BarGaugeControler ui2;
     public ClockControler ui3;
 
+
+    private Animator anim;
+
     private float maximumCoolTime;
     private float coolTime;
     // Use this for initialization
@@ -23,17 +26,28 @@ public class TestMagus : MonoBehaviour
         Debug.Log("s");
         maximumCoolTime = 15.0f;
         coolTime = 0;
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        anim.SetBool("IsDead", Input.GetKey("q"));
 
-        if (Input.GetKeyDown("a") && coolTime <= 0)
+        if (Input.GetKeyDown("a") && coolTime <= 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Cast"))
         {
+            anim.SetTrigger("Attack");
             coolTime = maximumCoolTime;
-            magic[0].CastSpell();
         }
+        anim.SetBool("IsWalking", Input.GetKey("w"));
+
+        if(anim.GetBool("IsWalking") && anim.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+        {
+            this.transform.position = this.transform.position + new Vector3(0, 0, -0.017f);
+        }
+
+
         ui.GaugeUp.setValue(maximumCoolTime, maximumCoolTime - coolTime);
         ui2.setValue(maximumCoolTime, coolTime);
         ui3.setValue(maximumCoolTime, coolTime);
