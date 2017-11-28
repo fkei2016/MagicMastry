17d69a06ba83de2773050b55184f7aac7c257b3b
TSITCL\s152100 <sasamoto2016@gmail.com>
2017-11-28 11:11:07 +0900

    ビルボードUIの追加

---
+++ MagicMastry/Assets/Scripts/System/UI/BillboardUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardUI : MonoBehaviour {
+    public NameDisplay nameDisplay;
+    public BarGaugeControler gauge;
+    public Camera targetCamera;
+	// Update is called once per frame
+	void Update () {
+        BillboardRotate();
+    }
+
+    //ビルボードのための回転（カメラのほうを見る
+    void BillboardRotate()
+    {
+        transform.LookAt(transform.position + targetCamera.transform.rotation * Vector3.back, targetCamera.transform.rotation * Vector3.up);
+    }
+
+    //名前表示の関数
+    public void setValue(float max, float value)
+    {
+        gauge.setValue(max, value);
+    }
+
+    //ゲージセットの関数
+    //最大値と現在値からゲージの値を変更
+    public void setName(string _name)
+    {
+        nameDisplay.setName(_name);
+    }
+}
---
+++ MagicMastry/Assets/Scripts/System/UI/NameDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameDisplay : MonoBehaviour {
+    Text enemyName;
+
+	// Use this for initialization
+	void Start () {
+
+        enemyName = this.GetComponent<Text>();
+    }
+
+    public void setName(string _name)
+    {
+        enemyName.text = _name;
+    }
+}
